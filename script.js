98356c99944f1a10c61de974d145c5dc5fb03aef
shradhachityal7-svg <shradhachityal7@gmail.com>
2026-04-17 16:45:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+function addTask(columnId) {
+  const taskText = prompt("Enter task:");
+  if (!taskText) return;
+
+  const task = document.createElement("div");
+  task.className = "task";
+  task.innerText = taskText;
+
+  document.querySelector(`#${columnId} .task-list`).appendChild(task);
+}
